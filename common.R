a75ad05a3a0664be31b409da4d7ba256857f9265
Danielle Navarro <d.navarro@unsw.edu.au>
2019-07-17 20:15:10 +1000

    first pass at flavour_quote() function (wonky on pdf output)

--- common.R
+++ common.R
@@ -7,3 +7,24 @@ eps <- TRUE
 colour <- TRUE
 width <- 12
 height <- 8
+
+# returns a list of expressions
+capture_dots <- function(...) {
+  as.list(substitute(list(...)))[-1L]
+}
+
+# code for flavour text
+flavour_quote <- function(..., auth) {
+  dots <- capture_dots(...)
+  dots <- lapply(dots, function(x){ 
+    paste0("&nbsp;&nbsp;&nbsp;&nbsp;", x)
+  })
+  cat('*')
+  cat(unlist(dots), sep="<br>\\newline")
+  cat("*<br>\\newline")
+  cat(paste0(
+    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--- ", 
+    auth, 
+    "<br>\\newline"
+  ))
+}
